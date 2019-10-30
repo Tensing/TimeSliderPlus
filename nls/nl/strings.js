c8425b196a6a80e1a06ef5cf91ff438bf89e17de
Jeroen Bloemscheer <JBloemscheer@tensing.com>
2019-10-30 16:46:17 +0100

    Added communication with Zandloper widget

---
+++ nls/nl/strings.js
@@ -0,0 +1,8 @@
+define({
+  "_widgetLabel": "Time Slider Plus",
+  "enableTips": "Klik hier om de tijdschuifregelaar weer te geven.",
+  "disableTips": "Er zijn geen lagen met tijdfunctionaliteit zichtbaar.",
+  "timeExtent": "${FROMTIME} naar ${ENDTIME}",
+  "layers": "Lagen: ",
+  "speedLabel": "Playback speed"
+});
